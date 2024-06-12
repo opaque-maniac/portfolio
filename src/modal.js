8608caa3ce48b116a1b526d25d58a4b1ce2060e2
opaque-maniac <mbithimark8@gmail.com>
2024-06-12 05:48:32 +0300

    Finished the rest of my portfolio website

---
+++ src/modal.js
@@ -0,0 +1,54 @@
+const modal = document.querySelector(".modal");
+const projectLinks = document.querySelectorAll(".proj-links");
+const modalProjects = document.querySelectorAll(".modal-project");
+const modalButton = document.querySelector("#modal-button");
+
+// blog-app, recipe-app, learning-log, marketplace
+// proj-links
+
+// Function to show modal
+const showModal = () => {
+  modal.classList.remove("hidden");
+  modal.setAttribute("aria-hidden", "false");
+  document.querySelector("body").style.overflow = "hidden";
+};
+
+// Function to hide modal
+const hideModal = () => {
+  modal.classList.add("hidden");
+  modal.setAttribute("aria-hidden", "true");
+  document.querySelector("body").style.overflow = "auto";
+};
+
+// Click event handler for project links
+const handleProjClick = (e) => {
+  e.preventDefault();
+  const name = e.currentTarget.dataset.name;
+  modalProjects.forEach((proj) => {
+    if (proj.dataset.name === name) {
+      proj.classList.remove("hidden");
+      proj.setAttribute("aria-hidden", "false");
+      showModal();
+    }
+  });
+};
+
+// Click event handler for modal close button
+const handleCloseModalClick = (e) => {
+  e.preventDefault();
+  hideModal();
+  modalProjects.forEach((project) => {
+    project.setAttribute("aria-hidden", "true");
+    if (!project.classList.contains("hidden")) {
+      project.classList.add("hidden");
+    }
+  });
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  projectLinks.forEach((link) => {
+    link.addEventListener("click", handleProjClick);
+  });
+
+  modalButton.addEventListener("click", handleCloseModalClick);
+});
